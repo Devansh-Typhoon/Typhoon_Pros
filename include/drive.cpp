2a8b91276becabc5ebd26d95cd3e000278821299
Devansh Jha <bigmandev26@gmail.com>
2022-12-23 18:27:15 +0000

    new pros code

---
+++ include/drive.cpp
@@ -0,0 +1,61 @@
+#include "main.h"
+
+//Helper Functions
+void resetMotorEncoders() {
+    driveFL.tare_position();
+    driveFR.tare_position();
+    driveBL.tare_position();
+    driveBR.tare_position();
+}
+
+void setDrive(float FL, float FR,float BL, float BR) {
+    driveFL.move(FL);
+    driveFR.move(FR);
+    driveBL.move(BL);
+    driveBR.move(BR);
+
+}
+//Flywheel Function
+void flywheel(void* ignore) {
+    fw1.move(127); 
+    fw2.move(127);
+}
+
+void shoot(int discs) {
+    cam.move_relative(360 * discs, 127);
+}
+//Autonomous Functions
+
+void translate(int units, int voltage) {
+//reset motor encoders
+resetMotorEncoders();
+//drive fwd/back until units reached
+while (driveFL.get_position() < units) {
+    setDrive(voltage,-voltage,-voltage,voltage);
+}
+//set drive back to neutral
+    setDrive(0,0,0,0);
+}
+
+void strafe(int units, int voltage) {
+    resetMotorEncoders();
+    //drive left/right until units are reached
+    while (driveFL.get_position() < units) {
+        setDrive(voltage,voltage,voltage,voltage);
+
+
+    }
+ //set drive back to neutral
+    setDrive(0,0,0,0);
+}
+
+void rotate(int units, int voltage) {
+    resetMotorEncoders();
+    // rotate until units are reached
+    while (driveFL.get_position() < units) {
+        setDrive(voltage,voltage,-voltage,-voltage);
+    }
+    //set drive back to neutral
+    setDrive(0,0,0,0);
+}
+
---
+++ include/drive.hpp
@@ -0,0 +1,15 @@
+#include "main.h"
+//Helper Functions
+void resetMotorEncoders();
+void setDrive();
+//Flywheel Function
+void flywheel();
+void shoot(int discs);
+//Autonomous Functions
+void translate(int units, int voltage);
+
+void strafe(int units, int voltage);
+
+void rotate(int units, int voltage);
+
+
