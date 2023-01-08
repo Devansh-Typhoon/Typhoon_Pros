0224626796c0cd6124f973944fe3373df4468a10
Devansh Jha <bigmandev26@gmail.com>
2023-01-08 13:18:33 +0000

    revert

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,6 @@
 #include "main.h"
 #include "drive.cpp"
 
-#include <iostream>
-#include <string>
-#include <sstream>
-
-int Brain_precision = 0, Console_precision = 0;
-
-const char* printToConsole_numberFormat() {
-  // look at the current precision setting to find the format string
-  switch(Console_precision){
-    case 0:  return "%.0f"; // 0 decimal places (1)
-    case 1:  return "%.1f"; // 1 decimal place  (0.1)
-    case 2:  return "%.2f"; // 2 decimal places (0.01)
-    case 3:  return "%.3f"; // 3 decimal places (0.001)
-    default: return "%f"; // use the print system default for everthing else
-  }
-}
-
 
 //cricket
 
@@ -167,10 +150,7 @@ void opcontrol() {
 
 	while (true) {
 	// Printing Flywheel Values to terminal
-	printf("%.1f", fw1.get_actual_velocity());
-  	printf(";");
-  	printf("%.1f", fw2.get_actual_velocity());
- 	printf("\n");
+	
 
 
 	
